fae31983793a245e028f8f320f132da19b050c16
raluca-puiu <36317063+raluca-puiu@users.noreply.github.com>
2018-03-12 19:40:33 -0400

    add

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+
+
+
+/* Twitter */
+window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    t._e = [];
+    t.ready = function(f) {
+        t._e.push(f);
+    };
+
+    return t;
+}(document, "script", "twitter-wjs"));
+
+
+$('.twitter-block').find('.twitter-timeline').contents().find('.timeline-Tweet').css('display', 'none');
+
